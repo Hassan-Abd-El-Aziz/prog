dbb794c696d95235eefb017a90900fcab86594c0
Hassan-Abd-El-Aziz <hassanabdelaziz940@gmail.com>
2025-09-24 16:03:18 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let one = document.querySelector(".one");
+let btn = document.querySelector("button");
+let hed = document.querySelector(".hed");
+
+function changcolorone() {
+  btn.style.background = "green";
+  hed.style.background = "green";
+}
+
+function changcolortwo() {
+  btn.style.background = "rgb(57, 3, 253)";
+  hed.style.background = "rgb(57, 3, 253)";
+}
+
+function changcolorthree() {
+  btn.style.background = "rgb(16, 164, 223)";
+  hed.style.background = "rgb(16, 164, 223)";
+}
